428d5d5715ff05a44c3fdea834fc0e989dd33ca5
Vinicius Ortis <viniciusortis03@outlook.com>
2024-11-20 08:29:10 -0300

    feat: incluindo funcionalidade de favoritar filmes

--- src/app/searchmovie/searchmovie.component.ts
+++ src/app/searchmovie/searchmovie.component.ts
@@ -1,22 +1,56 @@
 import { Component, inject } from '@angular/core';
 import { SearchMovieService } from './searchmovie.service';
-import { FavoritesService } from '../favoritemovie/favorite.service';
-
+import { CommonModule } from '@angular/common'; // Importa o CommonModule
 import { Filmes } from './Filmes';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-searchmovie',
   standalone: true,
-  imports: [FormsModule], // Adicionado FormsModule para usar ngModel
+  imports: [FormsModule , CommonModule],   // Adicionado FormsModule para usar ngModel
   templateUrl: './searchmovie.component.html',
   styleUrl: './searchmovie.component.css',
 })
 export class SearchmovieComponent {
   titulo: string = ''; // Inicialize como string vazia
   filmes!: Filmes;
+  favoritos: Filmes[] = [];
+
 
   private searchMovieServie = inject(SearchMovieService);
+
+  constructor(){
+    this.carregarFavoritos();
+  }
+
+   // Função para adicionar um filme aos favoritos
+   adicionarAosFavoritos(filme: Filmes) {
+    if (!this.eFavorito(filme)) {
+      this.favoritos.push(filme); // Adiciona o filme ao array local
+      this.salvarFavoritos(); // Salva no LocalStorage
+      alert(`${filme.Title} foi adicionado aos favoritos!`);
+    } else {
+      alert(`${filme.Title} já está nos favoritos.`);
+    }
+  }
+
+  // Verifica se um filme já está nos favoritos
+  eFavorito(filme: Filmes): boolean {
+    return this.favoritos.some((fav) => fav.imdbID === filme.imdbID);
+  }
+
+  // Salva os favoritos no LocalStorage
+  private salvarFavoritos() {
+    localStorage.setItem('favoritos', JSON.stringify(this.favoritos));
+  }
+
+  // Carrega os favoritos do LocalStorage
+  private carregarFavoritos() {
+    const storedFavoritos = localStorage.getItem('favoritos');
+    this.favoritos = storedFavoritos ? JSON.parse(storedFavoritos) : [];
+    console.log('Favoritos carregados:', this.favoritos);
+    console.log(this.favoritos[0].Title)
+  }
 
   onConsultaFilme() {
     if (this.titulo.trim()) {
@@ -31,23 +65,7 @@ export class SearchmovieComponent {
         },
       });
     } else {
-      console.error('O título do filme não foi informado');
+      alert('O título do filme não foi informado');
     }
-  }
-
-  filmesfavoritos: any[] = []; // Lista de filmes ou séries
-
-  constructor(private favoritesService: FavoritesService) {}
-
-  toggleFavorite(filmefav: any): void {
-    if (this.favoritesService.isFavorite(filmefav.id)) {
-      this.favoritesService.removeFavorite(filmefav.id);
-    } else {
-      this.favoritesService.addFavorite(filmefav);
-    }
-  }
-
-  isFavorite(filmefavId: string): boolean {
-    return this.favoritesService.isFavorite(filmefavId);
   }
 }
